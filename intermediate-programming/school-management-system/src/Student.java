27284f17cb55603aeae880ea4fc8ded244856b0c
CjayDoesCode <cjaycodes@protonmail.com>
2025-05-01 15:46:46 +0800

    Create Student class

---
+++ intermediate-programming/school-management-system/src/Student.java
@@ -0,0 +1,140 @@
+public class Student {
+    // Fields
+    private String id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    private Boolean pwd;
+    private String institute;
+    private String program;
+    private int year;
+    private String section;
+
+    // Constructors
+    public Student() {
+        id = "";
+        firstName = "";
+        middleName = "";
+        lastName = "";
+        sex = "";
+        pwd = false;
+        institute = "";
+        program = "";
+        year = 0;
+        section = "";
+    }
+
+    public Student(String id, String firstName, String middleName, String lastName,
+            String sex, Boolean pwd, String institute, String program, int year, String section) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.pwd = pwd;
+        this.institute = institute;
+        this.program = program;
+        this.year = year;
+        this.section = section;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Boolean getPwd() {
+        return pwd;
+    }
+
+    public String getInstitute() {
+        return institute;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setPwd(Boolean pwd) {
+        this.pwd = pwd;
+    }
+
+    public void setInstitute(String institute) {
+        this.institute = institute;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    // Methods
+    public String getFullName() {
+        String fullName = (!lastName.isEmpty() ? lastName : "")
+                + (!firstName.isEmpty() ? ", " + firstName : "")
+                + (!middleName.isEmpty() ? " " + middleName : "");
+
+        return fullName;
+    }
+
+    public String getFullName(int max) {
+        String fullName = (!lastName.isEmpty() ? lastName : "")
+                + (!firstName.isEmpty() ? ", " + firstName : "")
+                + (!middleName.isEmpty() ? " " + middleName : "");
+
+        return (((fullName.length() <= max)) ? fullName : fullName.substring(0, max - 3) + "...");
+    }
+}
