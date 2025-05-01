6977b1c26b20dbbe5b33763e68a70ae0eb593427
CjayDoesCode <cjaycodes@protonmail.com>
2025-05-01 15:53:55 +0800

    Create StudentManager class

---
+++ intermediate-programming/school-management-system/src/StudentManager.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class StudentManager {
+    // fields
+    private ArrayList<Student> studentList;
+
+    // constructor
+    public StudentManager() {
+        studentList = new ArrayList<Student>();
+        // load studentList
+    }
+
+    // methods
+    public void addStudent(Scanner scanner) {
+    }
+
+    public void removeStudent(Scanner scanner) {
+
+    }
+
+    public void updateStudent(Scanner scanner) {
+
+    }
+
+    public void listStudents() {
+    }
+
+    public void save() {
+
+    }
+}
