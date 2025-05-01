04ff9bf8b7d292896a851445be50c2f2f35e9605
CjayDoesCode <cjaycodes@protonmail.com>
2025-05-01 16:01:02 +0800

    Implement method to list all students

--- intermediate-programming/school-management-system/src/StudentManager.java
+++ intermediate-programming/school-management-system/src/StudentManager.java
@@ -24,6 +24,34 @@ public class StudentManager {
     }
 
     public void listStudents() {
+        if (studentList.isEmpty()) {
+            System.out.printf("\n[ Info ] Student list is empty.\n\n");
+            return;
+        }
+
+        System.out.printf("""
+
+                ================================================================================
+                                                  Student List
+                ================================================================================
+                ID       Name                  Sex     PWD    Institute  Program   Year  Section
+                ================================================================================
+                """);
+
+        for (Student student : studentList) {
+            System.out.printf("%-7s  %-20s  %-6s  %-5s  %-9s  %-8s  %-4d  %-7s\n",
+                    student.getId(),
+                    student.getFullName(20),
+                    student.getSex(),
+                    student.getPwd(),
+                    student.getInstitute(),
+                    student.getProgram(),
+                    student.getYear(),
+                    student.getSection());
+
+        }
+
+        System.out.printf("================================================================================\n\n");
     }
 
     public void save() {
