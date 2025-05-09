5dc860a3b63df7abffe940924d2df490bc3cb987
Hugo Molina <enetwarch@protonmail.com>
2025-05-09 22:28:44 +0800

    Added Modularity

--- intermediate-programming/school-management-system/src/Main.java
+++ intermediate-programming/school-management-system/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        StudentManager.loadStudentList();
-        Menu.displayMenu();
+        final StudentManager studentManager = new StudentManager(new ArrayList<>(), "students.csv", ",");
+        studentManager.loadStudentList();
+
+        final Menu menu = new Menu(studentManager);
+        menu.display();
     }
 }
